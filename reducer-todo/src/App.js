c9a03275d52e4b5545b7d987bfd47efdd6e2abd0
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-11-06 06:32:34 -0700

    built basics of form that uses reducer

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from 'react';
+import './css/app.css';
 
-function App() {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        todos: [...state.todos, { task: action.payload, completed: false }]
+      };
+    case "TOGGLE_TODO":
+      return {
+        todos: state.todos.map((item, index) => index === action.index ? { ...item, completed: !item.completed } : item)
+      };
+    default:
+      throw new Error("No action matched!");
+  }
+};
+
+const initialState = { todos: [] }
+
+const App = () => {
+  const [{ todos }, dispatch] = useReducer(reducer, initialState);
+  const [task, setTask] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          dispatch({ type: "ADD_TODO", payload: task });
+          setTask("");
+        }}
+      >
+        <input value={task} onChange={e => setTask(e.target.value)} />
+      </form>
+      {todos.map((item, index) => {
+        return (
+          <div
+            key={item.task}
+            onClick={() => dispatch({ type: "TOGGLE_TODO", index })}
+            style={{
+              textDecoration: item.completed ? "line-through" : ""
+            }}
+          >
+            {item.task}
+          </div>
+        )
+      })}
+
     </div>
   );
 }
